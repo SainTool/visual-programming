7b185dc9ebad42c8f43ea4e34d76f3ed8f362c92
SainTool <78474049+SainTool@users.noreply.github.com>
2022-02-17 13:46:53 +0700

    Add Program.cs

---
+++ lab_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace visual_programming
+{
+    class Program{
+        static void Main(string[] args) {
+            int[] mass = new int [4]{ 10,2,3,3 };
+            int n = 2;
+            Console.WriteLine(HW1.QueueTime(mass, n));
+        }
+    }
+    public class HW1 {
+        public static long QueueTime(int[] customers, int n) {
+            int[] cass = new int[n];
+            for (int i = 0; i < n; cass[i++] = 0) { }
+            foreach(int znach in customers) {
+                cass[0] += znach;
+                if(n!=1)
+                    Array.Sort(cass);
+            }
+            return cass[n-1];
+        }
+    }
+}
