ea35d95dcedd41e6355dc669887abfa6fe1fef8f
SainTool <78474049+SainTool@users.noreply.github.com>
2022-02-24 21:43:32 +0700

    не работает сорт

---
+++ lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Numb;
+using Except;
+
+namespace visual_programming
+{
+    class Program{
+        static void Main(string[] args) {
+            ushort t1 = ((ushort)331);
+            RomanNumber b1 = new RomanNumber(t1);
+            ushort t2 = ((ushort)152);
+            RomanNumber b2 = new RomanNumber(t2);
+            Console.WriteLine("331 = " + b1.ToString());
+            Console.WriteLine("152 = " + b2.ToString());
+            Console.WriteLine("Arabian \t\t|\tRoman");
+            Console.WriteLine("331 + 152 =  483 \t|\t" + b1.ToString() + " + " + b2.ToString() + " = " + RomanNumber.Add(b1, b2));
+            Console.WriteLine("331 - 152 =  179 \t|\t" + b1.ToString() + " - " + b2.ToString() + " = " + RomanNumber.Sub(b1, b2));
+            t1 = ((ushort)25);
+            b1 = new RomanNumber(t1);
+            t2 = ((ushort)5);
+            b2 = new RomanNumber(t2);
+            Console.WriteLine("25 * 5 = 125 \t\t|\t" + b1.ToString() + " * " + b2.ToString() + " = " + RomanNumber.Mul(b1, b2));
+            Console.WriteLine("25 / 5 = 5 \t\t|\t" + b1.ToString() + " / " + b2.ToString() + " = " + RomanNumber.Div(b1, b2));
+            short[] mass = new short[] { 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            RomanNumber[] massRN = new RomanNumber[mass.Length];
+            Console.WriteLine("Mass");
+            for (int i = 0; i < massRN.Length; i++)
+            {
+                massRN[i] = new RomanNumber((ushort)mass[i]);
+                Console.Write(massRN[i].ToString() + " ");
+            }
+            Console.WriteLine("\n Sorted mass");
+            Array.Sort(massRN);
+            for (int i = 0; i < massRN.Length; i++)
+            {
+                Console.Write(massRN[i].ToString() + " ");
+            }
+        }
+    }
+}
---
+++ lab2/src/RomanNumber.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using Except;
+
+namespace Numb
+{
+    public class RomanNumber : ICloneable, IComparable
+    {
+        //число
+        private ushort num;
+        //создаём алфавит чисел 
+        private static short[] Arabian = new short[]
+            {1000,900,500,400,100,90,50,40,10,9,5,4,1 };
+        private static string[] Roman = new string[]
+            {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
+
+        //Конструктор получает число n, которое должен представлять объект
+        //класса
+        public RomanNumber(ushort n)
+        {
+            if (n <= 0)
+            {
+                throw new RomanNumberException("!!! Number<=0 !!!");
+            }
+            num = n;
+        }
+        //Сложение римских чисел
+        public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
+        {
+            return new RomanNumber((ushort)(n1.num + n2.num));
+        }
+        //Вычитание римских чисел
+        public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1.num - n2.num <= 0)
+            {
+                throw new RomanNumberException("!!! Sub <=0 !!!");
+            }
+            return new RomanNumber((ushort)(n1.num - n2.num));
+        }
+        //Умножение римских чисел
+        public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
+        {
+            return new RomanNumber((ushort)(n1.num * n2.num));
+            throw new NotImplementedException();
+        }
+        //Целочисленное деление римских чисел
+        public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1.num / n2.num <=0)
+            {
+                throw new RomanNumberException("!!! Div <=0 !!!");
+            }
+            return new RomanNumber((ushort)(n1.num / n2.num));
+        }
+        //Возвращает строковое представление римского числа
+        public override string ToString()
+        {
+            StringBuilder RomanEND = new StringBuilder();
+            short n = ((short)num);
+            for (int i = 0; i < Roman.Length; i++)
+            {
+                while (n >= Arabian[i])
+                {
+                    n -= Arabian[i];
+                    RomanEND.Append(Roman[i]);
+                }
+            }
+            return RomanEND.ToString();
+        }
+        //Реализация интерфейса ICloneable
+        public object Clone()
+        {
+            return new RomanNumber(num);
+        }
+        //Реализация интерфейса IComparable
+        public int CompareTo(object? obj)
+        {
+            if (obj is RomanNumber person) {
+                short pp = ((short)person);
+                return (short)(num.CompareTo(person)); }
+            else throw new RomanNumberException("Некорректное значение параметра");
+        }
+
+        
+    }
+}
